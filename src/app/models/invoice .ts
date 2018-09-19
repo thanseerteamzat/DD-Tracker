452c88f4f742ca96784fcbe3dfc2c7d28384ee75
thanseerteamzat <thanseer@eduzat.com>
2018-09-19 16:57:10 +0530

    .

---
+++ src/app/models/invoice .ts
@@ -0,0 +1,15 @@
+export class Invoice {
+
+    invoiceId: string;
+    dbaNo: string;
+    CourseCode: string;
+    feesItem: string;
+    CenterCode: string;
+    CenterId: string;
+    dbaAmount: string;
+    feeAmount: string;
+    share: Number;
+    shareAmount: string;
+    isdbaEntered :boolean;
+    enteredby :string;
+}
