9186f31c06311193cf71603ebba35d1f9db17132
arshadteamzat <arshad@eduzat.com>
2018-10-04 10:22:41 +0700

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ import { EtsService } from './services/ets.service';
 export class AppComponent implements OnInit {
   title = 'DD Tracker';
   Date = '03/10/2018(6:00pm)';
-  version = 'V 3.1.3'
+  version = 'V 3.1.4'
   // userId:[];
   cookievalue;
   expiredDate: Date;
