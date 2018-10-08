c6e6c7556ddfe5381b1dff5b9c4ada2be10e4353
thanseerteamzat <thanseer@eduzat.com>
2018-10-08 19:25:47 +0530

    ..

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ import { EtsService } from './services/ets.service';
 })
 export class AppComponent implements OnInit {
   title = 'DD Tracker';
-  Date = '08/10/2018(6:00pm)';
+  Date = '08/10/2018(7:030pm)';
   version = 'V 3.2.12'
   // userId:[];
   cookievalue;
