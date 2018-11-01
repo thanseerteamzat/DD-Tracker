19e086248328a0ba28abdc5ed1e93bbcf2b086db
arshadteamzat <arshad@eduzat.com>
2018-11-01 19:47:06 +0700

    .

---
+++ src/app/sortby-date.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { SortbyDatePipe } from './sortby-date.pipe';
+
+describe('SortbyDatePipe', () => {
+  it('create an instance', () => {
+    const pipe = new SortbyDatePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
